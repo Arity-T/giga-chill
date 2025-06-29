cde54228d166a67ce2aa3305cb2b2ac29a1ea35a
Arity-T <tish.mainmail@gmail.com>
2025-06-29 13:56:35 +0300

    Минимальные стили для формы входа

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Card, Typography, Flex, Checkbox } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+
+import styles from './page.module.css';
+
+const { Title } = Typography;
 
 export default function LoginForm() {
   const onFinish = (values: any) => {
@@ -8,32 +13,38 @@ export default function LoginForm() {
   };
 
   return (
-    <Form
-      name="login"
-      onFinish={onFinish}
-      style={{ maxWidth: 300, margin: '0 auto' }}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Введите email!' }]}
-      >
-        <Input />
-      </Form.Item>
+    <div className={styles.container}>
+      <Card className={styles.loginCard}>
+        <Title level={2} className={styles.title}>
+          Вход в систему
+        </Title>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          style={{ maxWidth: 360 }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="login"
+            rules={[{ required: true, message: 'Введите логин!' }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Логин" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Введите пароль!' }]}
+          >
+            <Input prefix={<LockOutlined />} type="password" placeholder="Пароль" />
+          </Form.Item>
 
-      <Form.Item
-        label="Пароль"
-        name="password"
-        rules={[{ required: true, message: 'Введите пароль!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Войти
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Войти
+            </Button>
+            или <a href="">Зарегистрироваться!</a>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 }
