8f2c4d5b80db2e178a0771bcdad89f969ad1371f
Vladislav Gaar <gaarvlad@gmail.com>
2025-07-01 13:21:04 +0300

    Сделал миграцию БД

---
+++ backend/src/main/resources/migration/V1__init_main.sql
@@ -0,0 +1,85 @@
+-- Расширения
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto; -- Для UUID
+
+-- Перечисления
+
+CREATE TYPE event_role AS ENUM ('participant', 'admin', 'owner');
+
+CREATE TYPE task_status AS ENUM ('open', 'in work', 'under review', 'completed', 'canceled')
+
+-- Таблицы
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  login VARCHAR(16) NOT NULL UNIQUE, -- Согласовать и прописать ограничения
+  password_hash TEXT NOT NULL,
+  name VARCHAR(32) NOT NULL -- Согласовать и прописать ограничения
+);
+
+CREATE TABLE IF NOT EXISTS event (
+  event_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  description TEXT,
+  start_datetime TIMESTAMP DEFAULT NULL,
+  end_datetime TIMESTAMP DEFAULT NULL,
+  budget NUMERIC DEFAUL NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_in_event (
+  user_in_event_id SERIAL PRIMARY KEY,
+  user_id UUID NOT NULL REFERENCES users(user_id),
+  event_id UUID NOT NULL REFERENCES event(event_id),
+  role event_role NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS task (
+  task_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  event_id UUID NOT NULL REFERENCES event(event_id), 
+  author_id UUID NOT NULL REFERENCES users(user_id),
+  executor_id UUID DEFAULT NULL REFERENCES users(user_id),
+  actual_approval_id DEFAULT NULL REFERENCES task_approval(task_approval_id),
+  title TEXT, -- Ограничения
+  description TEXT,
+  status task_status NOT NULL,
+  deadline_datetime TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS task_approval (
+  task_approval_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- UUID или хватит SERIAL (будет ли видно на эндпоинтах)?
+  task_id UUID NOT NULL REFERENCES task(task_id) ON DELETE CASCADE, 
+  executor_comment TEXT DEFAULT NULL,
+  reviewer_comment TEXT DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS purchase_list (
+  purchase_list_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  task_id UUID NOT NULL REFERENCES task(task_id), -- Список же не может существовать без задачи?
+  event_id UUID NOT NULL REFERENCES event(event_id),
+  title TEXT NOT NULL, -- Можем сделать ещё и unique
+  budget NUMERIC DEFAULT NULL,
+  -- Дописать логику с purchase_approval и file_link
+);
+
+CREATE TABLE IF NOT EXISTS purchase (
+  purchase_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  purchase_list_id UUID NOT NULL REFERENCES purchase_list(purchase_list_id), -- Покупка не существует без задачи?
+  event_id UUID NOT NULL REFERENCES event(event_id),
+  title TEXT NOT NULL, -- Можно unique, можно ограничения
+  quantity INTEGER NOT NULL,
+  unit TEXT NOT NULL, -- Можно ограничения
+  is_purchased BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS consumer_in_list (
+  consumer_in_list_id SERIAL PRIMARY KEY,
+  user_id UUID NOT NULL REFERENCES users(user_id),
+  purchase_list_id UUID NOT NULL REFERENCES purchase_list(purchase_list_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS purchase_list_approval (
+  purchase_list_approval_id SERIAL PRIMARY KEY,
+  task_approval_id UUID NOT NULL REFERENCES task_approval(task_approval_id) ON DELETE CASCADE,
+  purchase_list_id UUID NOT NULL REFERENCES purchase_list(purchase_list_id) ON DELETE CASCADE,
+  budget NUMERIC NOT NULL,
+  file_link TEXT
+);
