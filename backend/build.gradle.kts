981a72a8a74357a8b2e8ca07d9afa68b35e59783
Vladislav Gaar <gaarvlad@gmail.com>
2025-07-01 22:33:46 +0300

    Мои тщетные попытки настроить flyway

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.flywaydb.flyway") version "11.10.0"
+	id("nu.studer.jooq") version "8.2"
 }
 
 group = "com.github.giga-chill"
@@ -23,20 +25,89 @@ repositories {
 	mavenCentral()
 }
 
+val flywayClasspath by configurations.creating
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.postgresql:postgresql:42.1.4")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.1")
+	implementation("org.jooq:jooq")
+
+	flywayClasspath("org.postgresql:postgresql:42.7.3")
+
+	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-gson:0.12.1")
+
 	compileOnly("org.projectlombok:lombok")
+
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	jooqGenerator("org.jooq:jooq-codegen")
+	jooqGenerator("org.jooq:jooq-meta")
+	jooqGenerator("org.postgresql:postgresql")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/gigachill"
+    user = "postgres"
+    password = "postgres"
+    schemas = arrayOf("public")
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
+}
+
+jooq {
+    configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(false) // чтобы не генерировать на каждой сборке (можно true)
+
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/gigachill"
+                    user = "postgres"
+                    password = "postgres"
+                }
+
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                    }
+
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+
+                    target.apply {
+                        packageName = "com.github.giga_chill.jooq"
+                        directory = "src/main/java"
+                    }
+                }
+            }
+        }
+    }
+}
+
