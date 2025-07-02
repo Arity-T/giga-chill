50f961731745232e07a51435c0c66af6e4ff778d
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-02 19:28:43 +0300

    Добавит DTO для получения данных о мероприятие и переименовал DTO для отправки

--- backend/src/main/java/com/github/giga_chill/gigachill/web/info/EventInfo.java
+++
@@ -1,5 +0,0 @@
-package com.github.giga_chill.gigachill.web.info;
-
-public record EventInfo(String event_id, String user_role, String title, String location, String start_datetime,
-                        String end_datetime, String description, Integer budget) {
-}
---
+++ backend/src/main/java/com/github/giga_chill/gigachill/web/info/RequestEventInfo.java
@@ -0,0 +1,5 @@
+package com.github.giga_chill.gigachill.web.info;
+
+public record RequestEventInfo(String title, String location, String start_datetime,
+                               String end_datetime, String description) {
+}
