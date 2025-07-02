cfdc405201d55ca3d23594271c3b296ed8ca8354
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-03 02:21:32 +0300

    фикс delete events/{event_id}

--- backend/src/main/java/com/github/giga_chill/gigachill/web/controller/EventsController.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/web/controller/EventsController.java
@@ -78,11 +78,10 @@ public class EventsController {
         return ResponseEntity.ok(toResponseEventInfo(event, eventService.getUserRoleInEvent(user.id, event.getEvent_id())));
     }
 
-    @PatchMapping("/{eventId}")
+    @DeleteMapping("/{eventId}")
     //TODO: Настроить подгрузку роли из бд
 //    @PreAuthorize("hasAnyRole('ROLE_OWNER')")
-    public ResponseEntity<Void> deleteEventById(@RequestBody RequestEventInfo requestEventInfo,
-                                                            Authentication authentication, @PathVariable String eventId){
+    public ResponseEntity<Void> deleteEventById(Authentication authentication, @PathVariable String eventId){
         //TODO: Добавить обработку 400
         User user = userAuthentication(authentication);
         if (eventService.getEventById(eventId) == null){
