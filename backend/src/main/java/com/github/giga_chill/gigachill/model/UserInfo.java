ee5a36f8f4361ca6a169f87f59589e96fe31d149
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-02 18:19:08 +0300

    Переместил UserInfo

--- backend/src/main/java/com/github/giga_chill/gigachill/model/UserInfo.java
+++
@@ -1,5 +0,0 @@
-package com.github.giga_chill.gigachill.model;
-
-import java.util.Set;
-
-public record UserInfo(String login, String name, String id, Set<Role> roles) {}
---
+++ backend/src/main/java/com/github/giga_chill/gigachill/model/info/UserInfo.java
@@ -0,0 +1,5 @@
+package com.github.giga_chill.gigachill.model.info;
+
+import com.github.giga_chill.gigachill.model.Role;
+
+public record UserInfo(String login, String name, String id) {}
