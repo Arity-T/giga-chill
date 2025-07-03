48c8e6ec3d0b65074e50928d1e3603415c9654fe
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-03 15:31:57 +0300

    Добавил Get events/{eventId}/participants

--- backend/src/main/java/com/github/giga_chill/gigachill/service/ParticipantsService.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/service/ParticipantsService.java
@@ -1,4 +1,32 @@
 package com.github.giga_chill.gigachill.service;
 
+
+import com.github.giga_chill.gigachill.model.Participant;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class ParticipantsService {
+
+    private final Map<String, List<Participant>> EVENT_PARTICIPANTS = new HashMap<>();
+
+
+    public List<Participant> getAllParticipantsByEventId(String eventId){
+        //TODO: связь с бд
+
+        //TEMPORARY:
+        if (!EVENT_PARTICIPANTS.containsKey(eventId)) {
+            return List.of();
+        }
+        return EVENT_PARTICIPANTS.get(eventId);
+    }
+
+
+
+
+
+
 }
--- backend/src/main/java/com/github/giga_chill/gigachill/web/controller/ParticipantsController.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/web/controller/ParticipantsController.java
@@ -1,4 +1,53 @@
 package com.github.giga_chill.gigachill.web.controller;
 
+
+import com.github.giga_chill.gigachill.exception.NotFoundException;
+import com.github.giga_chill.gigachill.model.Participant;
+import com.github.giga_chill.gigachill.model.User;
+import com.github.giga_chill.gigachill.service.EventService;
+import com.github.giga_chill.gigachill.service.InMemoryUserService;
+import com.github.giga_chill.gigachill.service.ParticipantsService;
+import com.github.giga_chill.gigachill.web.info.ParticipantInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("events")
+@RequiredArgsConstructor
 public class ParticipantsController {
+
+    private final EventService eventService;
+    private final InMemoryUserService inMemoryUserService;
+    private final ParticipantsService participantsService;
+
+
+    @GetMapping("/{eventId}/participants")
+    //TODO: Настроить подгрузку роли из бд
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_OWNER', ROLE_PARTICIPANT)")
+    public ResponseEntity<List<ParticipantInfo>> getParticipants(Authentication authentication,
+                                                                 @PathVariable String eventId){
+        User user = inMemoryUserService.userAuthentication(authentication);
+        if (eventService.getEventById(eventId) == null){
+            throw new NotFoundException("Мероприятие не найдено");
+        }
+        return ResponseEntity.ok(participantsService.getAllParticipantsByEventId(eventId)
+                .stream()
+                .map(this::toParticipantInfo)
+                .toList());
+    }
+
+
+
+    private ParticipantInfo toParticipantInfo(Participant participant){
+        return new ParticipantInfo(participant.getLogin(), participant.getName(),
+                participant.getId(), participant.getRole());
+    }
+
 }
