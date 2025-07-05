8e7c386c71fa7e2ef458be97a9f0ab0ea1f76803
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-05 14:41:24 +0300

    Обновил наследование

--- backend/src/main/java/com/github/giga_chill/gigachill/exception/ForbiddenException.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/exception/ForbiddenException.java
@@ -1,6 +1,6 @@
 package com.github.giga_chill.gigachill.exception;
 
-public class ForbiddenException extends Throwable{
+public class ForbiddenException extends RuntimeException{
     public ForbiddenException(String message){
         super(message);
     }
