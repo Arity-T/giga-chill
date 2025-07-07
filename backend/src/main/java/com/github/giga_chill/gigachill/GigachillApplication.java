a601e80df6518437c6cbff326425d70816b1159a
Vladislav Gaar <gaarvlad@gmail.com>
2025-07-07 19:57:52 +0300

    Добавил подгрузку переменных окружения в рантайме

--- backend/src/main/java/com/github/giga_chill/gigachill/GigachillApplication.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/GigachillApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.github.giga_chill.gigachill.config.DotenvInitializer;
+
 @SpringBootApplication
 @EnableConfigurationProperties()
 public class GigachillApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GigachillApplication.class, args);
+			SpringApplication app = new SpringApplication(GigachillApplication.class);
+			app.addInitializers(new DotenvInitializer());
+			app.run(args);
 	}
 
 }
