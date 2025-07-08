965b2fc2d26aa4148fbffeb6d94ee232b159ef6f
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-09 00:54:19 +0300

    список покупок DTO для фронта

---
+++ backend/src/main/java/com/github/giga_chill/gigachill/web/info/ConsumerInfo.java
@@ -0,0 +1,8 @@
+package com.github.giga_chill.gigachill.web.info;
+
+public record ConsumerInfo(String login,
+                           String name,
+                           String id,
+                           String user_role,
+                           Integer balance) {
+}
