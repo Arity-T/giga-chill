1dd48ed8d3b21ed264fd97f22e4e25477c362f14
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-08 22:56:40 +0300

    Убрал лишний код и пометки

--- backend/src/main/java/com/github/giga_chill/gigachill/service/EventService.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/service/EventService.java
@@ -17,64 +17,28 @@ public class EventService {
 
     private final EventDAO eventDAO;
 
-    //TEMPORARY:
-//    private final Map<String, Event> EVENTS = new HashMap<>();
-//    private final Map<String, Map<String, Event>> USER_EVENTS = new HashMap<>();
-
 
     public boolean isExisted(String eventId){
-        //TODO: Связь с бд
         return eventDAO.isExisted(eventId);
-
-
-        //TEMPORARY:
-//        return EVENTS.containsKey(eventId);
     }
 
     public Event getEventById(String eventId) {
-        //TODO: Связь с бд
         return toEntity(eventDAO.getEventById(eventId));
-
-        //TEMPORARY:
-//        return EVENTS.get(eventId);
     }
 
     public List<Event> getAllUserEvents(String userId) {
-        //TODO: Связь с бд
         return eventDAO.getAllUserEvents(userId).stream()
                 .map(this::toEntity).toList();
 
-        //TEMPORARY:
-//        if (!USER_EVENTS.containsKey(userId)) {
-//            return List.of();
-//        }
-//
-//        return new ArrayList<>(USER_EVENTS.get(userId).values());
     }
 
     public Event updateEvent(String eventId, RequestEventInfo requestEventInfo) {
-        //TODO: Связь с бд
         EventDTO event = new EventDTO(eventId, requestEventInfo.title(),
                 requestEventInfo.location(), requestEventInfo.start_datetime(), requestEventInfo.end_datetime(),
                 requestEventInfo.description(), BigDecimal.valueOf(0));
         eventDAO.updateEvent(eventId, event);
         return getEventById(eventId);
 
-
-        //TEMPORARY:
-//        Event event = EVENTS.get(eventId);
-//        Optional.ofNullable(requestEventInfo.title())
-//                .ifPresent(event::setTitle);
-//        Optional.ofNullable(requestEventInfo.location())
-//                .ifPresent(event::setLocation);
-//        Optional.ofNullable(requestEventInfo.start_datetime())
-//                .ifPresent(event::setStartDatetime);
-//        Optional.ofNullable(requestEventInfo.end_datetime())
-//                .ifPresent(event::setEndDatetime);
-//        Optional.ofNullable(requestEventInfo.description())
-//                .ifPresent(event::setDescription);
-//
-//        return event;
     }
 
 
@@ -82,25 +46,13 @@ public class EventService {
         Event event = new Event(UUID.randomUUID().toString(), requestEventInfo.title(),
                 requestEventInfo.location(), requestEventInfo.start_datetime(), requestEventInfo.end_datetime(),
                 requestEventInfo.description(), BigDecimal.valueOf(0));
-        //TODO: Связь с бд
 
         eventDAO.createEvent(userId, toDto(event));
         return event;
-
-
-        //TEMPORARY:
-//        EVENTS.put(event.getEventId(), event);
-//        USER_EVENTS.computeIfAbsent(userId, value -> new HashMap<>()).put(event.getEventId(), event);
-//        return event;
     }
 
     public void deleteEvent(String eventId) {
-        //TODO: Связь с бд
         eventDAO.deleteEvent(eventId);
-
-        //TEMPORARY:
-//        EVENTS.remove(eventId);
-//        USER_EVENTS.get(userId).remove(eventId);
     }
 
     private Event toEntity(EventDTO eventDTO){
--- backend/src/main/java/com/github/giga_chill/gigachill/service/ParticipantsService.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/service/ParticipantsService.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -20,94 +17,35 @@ public class ParticipantsService {
     private final Environment env;
     private final ParticipantDAO participantDAO;
 
-    //TEMPORARY:
-//    private final Map<String, List<Participant>> EVENT_PARTICIPANTS = new HashMap<>();
-
     public List<Participant> getAllParticipantsByEventId(String eventId) {
-        //TODO: связь с бд
         return participantDAO.getAllParticipantsByEventId(eventId).stream()
                 .map(this::toEntity)
                 .toList();
-
-
-
-        //TEMPORARY:
-//        if (!EVENT_PARTICIPANTS.containsKey(eventId)) {
-//            return List.of();
-//        }
-//        return EVENT_PARTICIPANTS.get(eventId);
     }
 
     public Participant addParticipantToEvent(String eventId, User user) {
-        //TODO: связь с бд
         Participant participant = new Participant(user.id, user.login, user.name,
                 env.getProperty("roles.participant").toString());
 
         participantDAO.addParticipantToEvent(eventId, toDto(participant));
         return participant;
-        //TEMPORARY:
-//        EVENT_PARTICIPANTS.get(eventId).add(participant);
-//        return participant;
     }
 
-    //Может и не нужно
-//    public void createEvent(String eventId, User user) {
-//        //TODO: связь с бд
-//        Participant participant = new Participant(user.id, user.login, user.name, env.getProperty("roles.owner").toString());
-//        participantDAO.
-//
-//        //TEMPORARY:
-////        Participant participant = new Participant(user.id, user.login, user.name, env.getProperty("roles.owner").toString());
-////        EVENT_PARTICIPANTS.put(eventId, new ArrayList<>());
-////        EVENT_PARTICIPANTS.get(eventId).add(participant);
-//    }
-
     public void deleteParticipant(String eventId, String participantId) {
-        //TODO: связь с бд
         participantDAO.deleteParticipant(eventId, participantId);
-
-        //TEMPORARY:
-//        EVENT_PARTICIPANTS.get(eventId).removeIf(item -> participantId.equals(item.getId()));
     }
 
     public boolean isParticipant(String eventId, String userId) {
-        //TODO: связь с бд
         return participantDAO.isParticipant(eventId, userId);
-
-        //TEMPORARY:
-//        return EVENT_PARTICIPANTS.get(eventId).stream()
-//                .anyMatch(item -> userId.equals(item.getId()));
     }
 
     public Participant updateParticipantRole(String eventId, String participantId, String role) {
-        //TODO: связь с бд
-        //TODO: менять роль у себя
-        //TODO: запретить изменять роль owner
-
         participantDAO.updateParticipantRole(eventId, participantId, role);
         return toEntity(participantDAO.getParticipantById(eventId, participantId));
-
-//        //TEMPORARY:
-//
-//        Participant participant = EVENT_PARTICIPANTS.get(eventId)
-//                .stream()
-//                .filter(item -> participantId.equals(item.getId()))
-//                .findFirst()
-//                .orElse(null);
-//        participant.setRole(role);
-//        return participant;
     }
 
     public String getParticipantRoleInEvent(String eventId, String participantId) {
-        //TODO: Связь с бд
         return participantDAO.getParticipantRoleInEvent(eventId, participantId);
-
-        //TEMPORARY:
-//        return EVENT_PARTICIPANTS.get(eventId)
-//                .stream()
-//                .filter(item -> participantId.equals(item.getId()))
-//                .findFirst()
-//                .orElse(null).getRole();
     }
 
     public boolean isOwner(String eventId, String participantId) {
