38b03ae00c10adecd10eeed34c84836a38b81c24
Vladislav Gaar <gaarvlad@gmail.com>
2025-07-10 11:59:30 +0300

    Поменял тип поля quantity на BigDecimal

--- backend/src/main/java/com/github/giga_chill/gigachill/data/transfer/object/ShoppingItemDTO.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/data/transfer/object/ShoppingItemDTO.java
@@ -2,9 +2,11 @@ package com.github.giga_chill.gigachill.data.transfer.object;
 
 import jakarta.annotation.Nullable;
 
+import java.math.BigDecimal;
+
 public record ShoppingItemDTO(String shoppingItemId,
                               @Nullable String title,
-                              @Nullable Integer quantity,
+                              @Nullable BigDecimal quantity,
                               @Nullable String unit,
                               @Nullable Boolean isPurchased) {
 }
