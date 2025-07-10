303268f9cb3c9781ffcbb6b4bfd25b23b9507cfc
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-10 16:03:16 +0300

    Тип id с String на UUID в логгере

--- backend/src/main/java/com/github/giga_chill/gigachill/aspect/ShoppingListsServiceLoggerAspect.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/aspect/ShoppingListsServiceLoggerAspect.java
@@ -10,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 @Aspect
 @RequiredArgsConstructor
@@ -20,12 +22,12 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.getAllShoppingListsFromEvent(..)) " +
             "&& args(eventId)")
-    public void getAllShoppingListsFromEvent(String eventId) {
+    public void getAllShoppingListsFromEvent(UUID eventId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.getShoppingListById(..)) " +
             "&& args(eventId, shoppingListId)")
-    public void getShoppingListById(String eventId, String shoppingListId) {
+    public void getShoppingListById(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.createShoppingList(..))")
@@ -34,70 +36,70 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.updateShoppingList(..)) " +
             "&& args(eventId, shoppingListId, ..)")
-    public void updateShoppingList(String eventId, String shoppingListId) {
+    public void updateShoppingList(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.deleteShoppingList(..)) " +
             "&& args(eventId, shoppingListId)")
-    public void deleteShoppingList(String eventId, String shoppingListId) {
+    public void deleteShoppingList(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.addShoppingItem(..)) " +
             "&& args(eventId, shoppingListId, ..)")
-    public void addShoppingItem(String eventId, String shoppingListId) {
+    public void addShoppingItem(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.updateShoppingItem(..)) " +
             "&& args(eventId, shoppingListId, shoppingItemId, ..)")
-    public void updateShoppingItem(String eventId, String shoppingListId, String shoppingItemId) {
+    public void updateShoppingItem(UUID eventId, UUID shoppingListId, UUID shoppingItemId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.deleteShoppingItemFromShoppingList(..)) " +
             "&& args(eventId, shoppingListId, shoppingItemId)")
-    public void deleteShoppingItemFromShoppingList(String eventId, String shoppingListId, String shoppingItemId) {
+    public void deleteShoppingItemFromShoppingList(UUID eventId, UUID shoppingListId, UUID shoppingItemId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.updateShoppingItemStatus(..)) " +
             "&& args(eventId, shoppingListId, shoppingItemId, status)")
-    public void updateShoppingItemStatus(String eventId, String shoppingListId, String shoppingItemId,
+    public void updateShoppingItemStatus(UUID eventId, UUID shoppingListId, UUID shoppingItemId,
                                          boolean status) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.getShoppingItemById(..)) " +
             "&& args(eventId, shoppingListId, shoppingItemId)")
-    public void getShoppingItemById(String eventId, String shoppingListId,
-                                    String shoppingItemId) {
+    public void getShoppingItemById(UUID eventId, UUID shoppingListId,
+                                    UUID shoppingItemId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.updateShoppingListConsumers(..)) " +
             "&& args(eventId, shoppingListId, ..)")
-    public void updateShoppingListConsumers(String eventId, String shoppingListId) {
+    public void updateShoppingListConsumers(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.getShoppingListStatus(..)) " +
             "&& args(eventId, shoppingListId)")
-    public void getShoppingListStatus(String eventId, String shoppingListId) {
+    public void getShoppingListStatus(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.isExisted(..)) " +
             "&& args(eventId, shoppingListId)")
-    public void isExisted(String eventId, String shoppingListId) {
+    public void isExisted(UUID eventId, UUID shoppingListId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.isConsumer(..)) " +
             "&& args(eventId, shoppingListId, consumerId)")
-    public void isConsumer(String eventId, String shoppingListId, String consumerId) {
+    public void isConsumer(UUID eventId, UUID shoppingListId, UUID consumerId) {
     }
 
     @Pointcut("execution(public * com.github.giga_chill.gigachill.service.ShoppingListsService.isShoppingItemExisted(..)) " +
             "&& args(eventId, shoppingItemId)")
-    public void isShoppingItemExisted(String eventId, String shoppingItemId) {
+    public void isShoppingItemExisted(UUID eventId, UUID shoppingItemId) {
     }
 
 
     @Around("getAllShoppingListsFromEvent(eventId)")
     public Object logGetAllShoppingListsFromEvent(ProceedingJoinPoint proceedingJoinPoint,
-                                                  String eventId) throws Throwable {
+                                                  UUID eventId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getGET_COLOR() + "Event shopping lists with id: {} received"
@@ -110,8 +112,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("getShoppingListById(eventId, shoppingListId)")
     public Object logGetShoppingListById(ProceedingJoinPoint proceedingJoinPoint,
-                                         String eventId,
-                                         String shoppingListId) throws Throwable {
+                                         UUID eventId,
+                                         UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getGET_COLOR() + "Shopping list with id: {} received"
@@ -136,8 +138,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("updateShoppingList(eventId, shoppingListId)")
     public Object logUpdateShoppingList(ProceedingJoinPoint proceedingJoinPoint,
-                                        String eventId,
-                                        String shoppingListId) throws Throwable {
+                                        UUID eventId,
+                                        UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getPATCH_COLOR() + "Shopping list with id: {} was updated"
@@ -150,8 +152,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("deleteShoppingList(eventId, shoppingListId)")
     public Object logDeleteShoppingList(ProceedingJoinPoint proceedingJoinPoint,
-                                        String eventId,
-                                        String shoppingListId) throws Throwable {
+                                        UUID eventId,
+                                        UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getDELETE_COLOR() + "Shopping list with id: {} was deleted"
@@ -164,8 +166,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("addShoppingItem(eventId, shoppingListId)")
     public Object logAddShoppingItem(ProceedingJoinPoint proceedingJoinPoint,
-                                     String eventId,
-                                     String shoppingListId) throws Throwable {
+                                     UUID eventId,
+                                     UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getPOST_COLOR() + "Shopping item with id: {} was added to shopping list with id: {}"
@@ -178,9 +180,9 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("updateShoppingItem(eventId, shoppingListId, shoppingItemId)")
     public Object logUpdateShoppingItem(ProceedingJoinPoint proceedingJoinPoint,
-                                        String eventId,
-                                        String shoppingListId,
-                                        String shoppingItemId) throws Throwable {
+                                        UUID eventId,
+                                        UUID shoppingListId,
+                                        UUID shoppingItemId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getPATCH_COLOR() + "Shopping item with id: {} was updated"
@@ -193,9 +195,9 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("deleteShoppingItemFromShoppingList(eventId, shoppingListId, shoppingItemId)")
     public Object logDeleteShoppingItemFromShoppingList(ProceedingJoinPoint proceedingJoinPoint,
-                                                        String eventId,
-                                                        String shoppingListId,
-                                                        String shoppingItemId) throws Throwable {
+                                                        UUID eventId,
+                                                        UUID shoppingListId,
+                                                        UUID shoppingItemId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getDELETE_COLOR() + "Shopping item with id: {} was deleted from shopping list with id: {}"
@@ -208,13 +210,13 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("updateShoppingItemStatus(eventId, shoppingListId, shoppingItemId, status)")
     public Object logUpdateShoppingItemStatus(ProceedingJoinPoint proceedingJoinPoint,
-                                              String eventId,
-                                              String shoppingListId,
-                                              String shoppingItemId,
+                                              UUID eventId,
+                                              UUID shoppingListId,
+                                              UUID shoppingItemId,
                                               boolean status) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
-            LOGGER.info(loggerColorConfig.getPATCH_COLOR() + "Shopping item status with id: {} is {} now"
+            LOGGER.info(loggerColorConfig.getPATCH_COLOR() + "Shopping item with id: {} was bought: {}"
                     + loggerColorConfig.getRESET_COLOR(), shoppingItemId, status);
             return result;
         } catch (Throwable ex) {
@@ -225,9 +227,9 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("getShoppingItemById(eventId, shoppingListId, shoppingItemId)")
     public Object logGetShoppingItemById(ProceedingJoinPoint proceedingJoinPoint,
-                                         String eventId,
-                                         String shoppingListId,
-                                         String shoppingItemId) throws Throwable {
+                                         UUID eventId,
+                                         UUID shoppingListId,
+                                         UUID shoppingItemId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getGET_COLOR() + "Shopping item with id: {} was received"
@@ -240,8 +242,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("updateShoppingListConsumers(eventId, shoppingListId)")
     public Object logUpdateShoppingListConsumers(ProceedingJoinPoint proceedingJoinPoint,
-                                                 String eventId,
-                                                 String shoppingListId) throws Throwable {
+                                                 UUID eventId,
+                                                 UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getPUT_COLOR() + "Shopping list with id: {} consumers was updated"
@@ -254,8 +256,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("getShoppingListStatus(eventId, shoppingListId)")
     public Object logGetShoppingListStatus(ProceedingJoinPoint proceedingJoinPoint,
-                                           String eventId,
-                                           String shoppingListId) throws Throwable {
+                                           UUID eventId,
+                                           UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             LOGGER.info(loggerColorConfig.getGET_COLOR() + "Shopping list with id: {} has status {}"
@@ -268,8 +270,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("isExisted(eventId, shoppingListId)")
     public Object logIsExisted(ProceedingJoinPoint proceedingJoinPoint,
-                               String eventId,
-                               String shoppingListId) throws Throwable {
+                               UUID eventId,
+                               UUID shoppingListId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             if ((Boolean) result) {
@@ -289,9 +291,9 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("isConsumer(eventId, shoppingListId, consumerId)")
     public Object logIsConsumer(ProceedingJoinPoint proceedingJoinPoint,
-                                String eventId,
-                                String shoppingListId,
-                                String consumerId) throws Throwable {
+                                UUID eventId,
+                                UUID shoppingListId,
+                                UUID consumerId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             if ((Boolean) result) {
@@ -313,8 +315,8 @@ public class ShoppingListsServiceLoggerAspect {
 
     @Around("isShoppingItemExisted(eventId, shoppingItemId)")
     public Object logIsShoppingItemExisted(ProceedingJoinPoint proceedingJoinPoint,
-                                           String eventId,
-                                           String shoppingItemId) throws Throwable {
+                                           UUID eventId,
+                                           UUID shoppingItemId) throws Throwable {
         try {
             Object result = proceedingJoinPoint.proceed();
             if ((Boolean) result) {
