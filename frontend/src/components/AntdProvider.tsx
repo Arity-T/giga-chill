607596d9329cf35faca026cb13d008d4cccc9d8b
Arity-T <tish.mainmail@gmail.com>
2025-07-10 23:31:40 +0300

    Установил русскую локаль глобально

--- frontend/src/components/AntdProvider.tsx
+++ frontend/src/components/AntdProvider.tsx
@@ -6,11 +6,20 @@ import "@ant-design/v5-patch-for-react-19";
 
 import { StyleProvider } from "@ant-design/cssinjs";
 import { ConfigProvider, App } from "antd";
+import locale from 'antd/locale/ru_RU';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
 import type { FC, PropsWithChildren } from "react";
+
+// Настраиваем dayjs на русский язык глобально
+dayjs.locale('ru');
 
 const AntdProvider: FC<PropsWithChildren> = ({ children }) => (
   <StyleProvider layer>
-    <ConfigProvider theme={{ cssVar: true, hashed: false }}>
+    <ConfigProvider
+      theme={{ cssVar: true, hashed: false }}
+      locale={locale}
+    >
       <App>{children}</App>
     </ConfigProvider>
   </StyleProvider>
