003a96fe158ab6c4d571458e0f1b78c17cfd00ef
Vladislav Gaar <gaarvlad@gmail.com>
2025-07-14 17:09:51 +0300

    Добавил методы для работы с invite_link в EventRepository

--- backend/src/main/java/com/github/giga_chill/gigachill/repository/EventRepository.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/repository/EventRepository.java
@@ -1,5 +1,6 @@
 package com.github.giga_chill.gigachill.repository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.jooq.DSLContext;
 import com.github.giga_chill.jooq.generated.tables.records.EventsRecord;
@@ -9,13 +10,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@RequiredArgsConstructor
 public class EventRepository {
   private final DSLContext dsl;
-
-  // через конструктор получаем DSLContext — он будет создан в JooqConfig
-  public EventRepository(DSLContext dsl) {
-      this.dsl = dsl;
-  }
 
   public void save(EventsRecord record) {
     dsl.insertInto(Events.EVENTS)
@@ -37,5 +34,20 @@ public class EventRepository {
         .set(Events.EVENTS.IS_DELETED, true)
         .where(Events.EVENTS.EVENT_ID.eq(eventId))
         .execute();
+  }
+
+  public void updateInviteLink(UUID eventId, UUID inviteLinkUuid) {
+    dsl.update(Events.EVENTS)
+            .set(Events.EVENTS.INVITE_LINK, inviteLinkUuid)
+            .where(Events.EVENTS.EVENT_ID.eq(eventId))
+            .execute();
+  }
+
+  public boolean isCorrectLink(UUID eventId, UUID linkUuid) {
+    return dsl.fetchExists(
+            dsl.selectFrom(Events.EVENTS)
+            .where(Events.EVENTS.EVENT_ID.eq(eventId))
+            .and(Events.EVENTS.INVITE_LINK.eq(linkUuid))
+    );
   }
 }
