84837a86911a9a4356719bc6524b2aee51fede4b
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-15 16:34:41 +0300

    Обновил контроллер задач в соответствии с новым API

--- backend/src/main/java/com/github/giga_chill/gigachill/util/UuidUtils.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/util/UuidUtils.java
@@ -5,7 +5,7 @@ import com.github.giga_chill.gigachill.exception.BadRequestException;
 import java.util.UUID;
 
 public final class UuidUtils {
-    private UUID safeUUID(String raw) {
+    public static UUID safeUUID(String raw) {
         try {
             return UUID.fromString(raw);
         } catch (IllegalArgumentException e) {
