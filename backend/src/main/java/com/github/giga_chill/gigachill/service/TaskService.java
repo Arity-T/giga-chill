77976cc82fc207a1dc9c9be3f515f299bc080b9e
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-15 15:00:46 +0300

    hotfix 2 issue #19

--- backend/src/main/java/com/github/giga_chill/gigachill/service/TaskService.java
+++ backend/src/main/java/com/github/giga_chill/gigachill/service/TaskService.java
@@ -141,17 +141,18 @@ public class TaskService {
 
     public Map<String, Boolean> taskPermissions(UUID eventId, UUID taskId, UUID userId) {
         Map<String, Boolean> permissions = new HashMap<>();
-        boolean canEdit = true;
-        boolean canTakeItToWork = true;
+        var canEdit = true;
+        var canTakeItToWork = false;
+        var executorId = getExecutorId(taskId);
         if (getTaskStatus(taskId).equals(env.getProperty("task_status.completed"))) {
             canEdit = false;
         }
         if (participantsService.isParticipantRole(eventId, userId) && !isAuthor(taskId, userId)) {
             canEdit = false;
         }
-        if (!getTaskStatus(taskId).equals(env.getProperty("task_status.open"))
-                || getExecutorId(taskId) != null) {
-            canTakeItToWork = false;
+        if (getTaskStatus(taskId).equals(env.getProperty("task_status.open"))
+                && (getExecutorId(taskId) == null || executorId.equals(userId))) {
+            canTakeItToWork = true;
         }
         permissions.put("can_edit", canEdit);
         permissions.put("can_take_in_work", canTakeItToWork);
