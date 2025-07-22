e408b780ddafa9c8f39a43d0636e011bf5c7917f
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-07-23 01:59:35 +0300

    Подключил маппер

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -45,6 +45,10 @@ spotless {
 
 // === Зависимости приложения и тестов ===
 dependencies {
+    // Mapper
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
