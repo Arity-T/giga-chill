6413956168dd961e9e89a8bead3f2e5cee815fb8
Arity-T <tish.mainmail@gmail.com>
2025-07-28 17:28:57 +0300

    Обновил команды для участников

--- e2e-tests/cypress/support/commands/participants.ts
+++ e2e-tests/cypress/support/commands/participants.ts
@@ -6,30 +6,45 @@
 // Custom command для добавления участника по логину
 Cypress.Commands.add('addParticipantByLoginUI', (username) => {
     // Переходим на вкладку участников
-    cy.contains('.ant-menu-item a', 'Участники').click({ force: true });
+    cy.url().then((url) => {
+        // TODO: переделать на использование конфига
+        if (!url.includes("/participants")) {
+            cy.contains('.ant-menu-item a', 'Участники').click();
+        }
+    });
+
+    // Проверяем, что таблица с участниками загружена
+    cy.contains('table', 'Организатор').should('be.visible');
+
+    // Проверяем, что участник ещё не был добавлен
+    cy.contains('tr', username).should('not.exist');
 
     // Ждём загрузки и нажимаем кнопку добавления участника
     cy.contains('button', 'Добавить участника').should('be.visible').click();
 
     // В появившемся модальном окне вводим логин пользователя и нажимаем кнопку добавления
-    cy.contains('.ant-modal-content', 'Добавить участника')
+    cy.contains('.ant-modal-content', 'Добавить участника').should('be.visible')
         .within(() => {
             cy.get('input[placeholder="Введите логин пользователя"]')
                 .type(username)
                 .should('have.value', username);
 
-            // Подтверждаем добавление
             cy.contains('button', 'Добавить участника').should('be.visible').click();
         });
 
-    // Ждём завершения операции
+    // Проверяем, что участник добавлен
     cy.contains('tr', username).should('exist');
 });
 
 // Custom command для изменения роли участника по имени
 Cypress.Commands.add('changeParticipantRoleByNameUI', (participantName, newRole) => {
-    // Открываем вкладку «Участники»
-    cy.contains('.ant-menu-item a', 'Участники').click({ force: true });
+    // Переходим на вкладку участников
+    cy.url().then((url) => {
+        // TODO: переделать на использование конфига
+        if (!url.includes("/participants")) {
+            cy.contains('.ant-menu-item a', 'Участники').click();
+        }
+    });
 
     // Находим строку и сохраняем её в алиас
     cy.contains('tr', participantName).as('participantRow');
