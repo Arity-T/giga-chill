0219eb0f2a75310ea192b8db48d3d18bc24a8b80
Arity-T <tish.mainmail@gmail.com>
2025-08-18 22:55:22 +0300

    Убрал ещё один Warning Gradle

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "gigachill"
+
+// https://docs.gradle.org/8.14.2/userguide/toolchains.html#sub:download_repositories
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
+}
